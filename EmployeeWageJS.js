c5c200891ed6e598302ee44f86ddd0a43661365b
ashishmashal <ashishmashal5@gmail.com>
2022-06-08 22:45:31 +0530

    Added:Claculate Daily Wage For Multiple Days.

--- EmployeeWageJS.js
+++ EmployeeWageJS.js
@@ -1,26 +1,29 @@
 const PART_TIME_HRs=4;
 const FULL_TIME_HRs=8;
 const WAGE_PER_HRs=20;
+const NUM_OF_WORKING_DAYS = 20;
 
 
 function getWorkingHrs(employeeCheck){
 switch(employeeCheck){
     case 0:
-        console.log(" Employee is Present for Part Time ");
+        // console.log(" Employee is Present for Part Time ");
         return PART_TIME_HRs;
         break;
     case 1:
-        console.log(" Employee is Present for Full Time ");
+        // console.log(" Employee is Present for Full Time ");
         return FULL_TIME_HRs;
         break;
     default:
-        console.log("Employee is ABSENT ");
+        // console.log("Employee is ABSENT ");
     return 0;    
         break;        
 }
 }
-
-let employeeCheck = Math.floor(Math.random()*10)%3;
-let emphrs = getWorkingHrs(employeeCheck)
-let empWage = emphrs * WAGE_PER_HRs;
-console.log("Employee Hrs is:- "+emphrs+ " \nEmploye wage is :- " +empWage);
+let totalEmpHrs = 0;
+for(let day = 0; day < NUM_OF_WORKING_DAYS; day++){
+    let employeeCheck = Math.floor(Math.random()*10) % 3;
+    totalEmpHrs += getWorkingHrs(employeeCheck);
+}
+let empWage = totalEmpHrs * WAGE_PER_HRs;
+console.log("Total Employee Hrs is:- "+totalEmpHrs+ " \nEmploye wage is :- " +empWage);
