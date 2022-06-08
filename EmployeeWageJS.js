80e9560cfcb57cd72098c563f54540b30daa9ec1
ashishmashal <ashishmashal5@gmail.com>
2022-06-08 22:40:04 +0530

    Added:Calculated Employee Daily wages is added

--- EmployeeWageJS.js
+++ EmployeeWageJS.js
@@ -1,13 +1,25 @@
 console.log("Welcome to the employee wage programs :-) ");
 
-const IS_ABSENT = 0;
-let employeeCheck = Math.floor(Math.random() * 10 ) % 2;
 
-//Checking employee is present or absent
-//Present=1    Absent=0
+const PART_TIME_HRs=4;
+const FULL_TIME_HRs=8;
+const WAGE_PER_HRs=20;
 
-if(employeeCheck == IS_ABSENT){
-    console.log("Employee is ABSENT ");
-}else{
-    console.log("Employee is PRESENT");
+let emphrs = 0;
+let employeeCheck = Math.floor(Math.random()*10)%3;
+switch(employeeCheck){
+    case 0:
+        console.log("\nUC2 =>Employee is Present for Part Time ");
+        emphrs = PART_TIME_HRs;
+        break;
+    case 1:
+        console.log("\nUC2 => Employee is Present for Full Time ");
+        emphrs = FULL_TIME_HRs;
+        break;
+    default:
+        emphrs = 0;
+        console.log("\nUC2 =>Employee is ABSENT ");
+        break;        
 }
+let empWage = emphrs * WAGE_PER_HRs;
+console.log("Employe wage is :- " +empWage);
