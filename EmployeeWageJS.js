4221eb8a8fdfd539eb2df72be1414fc7777144f7
ashishmashal <ashishmashal5@gmail.com>
2022-06-08 22:42:32 +0530

    Added:Modified the code by using functions.

--- EmployeeWageJS.js
+++ EmployeeWageJS.js
@@ -1,25 +1,26 @@
-console.log("Welcome to the employee wage programs :-) ");
-
-
 const PART_TIME_HRs=4;
 const FULL_TIME_HRs=8;
 const WAGE_PER_HRs=20;
 
-let emphrs = 0;
-let employeeCheck = Math.floor(Math.random()*10)%3;
+
+function getWorkingHrs(employeeCheck){
 switch(employeeCheck){
     case 0:
-        console.log("\nUC2 =>Employee is Present for Part Time ");
-        emphrs = PART_TIME_HRs;
+        console.log(" Employee is Present for Part Time ");
+        return PART_TIME_HRs;
         break;
     case 1:
-        console.log("\nUC2 => Employee is Present for Full Time ");
-        emphrs = FULL_TIME_HRs;
+        console.log(" Employee is Present for Full Time ");
+        return FULL_TIME_HRs;
         break;
     default:
-        emphrs = 0;
-        console.log("\nUC2 =>Employee is ABSENT ");
+        console.log("Employee is ABSENT ");
+    return 0;    
         break;        
 }
+}
+
+let employeeCheck = Math.floor(Math.random()*10)%3;
+let emphrs = getWorkingHrs(employeeCheck)
 let empWage = emphrs * WAGE_PER_HRs;
-console.log("Employe wage is :- " +empWage);
+console.log("Employee Hrs is:- "+emphrs+ " \nEmploye wage is :- " +empWage);
